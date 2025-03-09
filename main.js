8644e6c593d1b15e4a0665e22116ac10a62bfe43
Juho Lehto <284658+lehtoj@users.noreply.github.com>
2025-03-09 10:35:45 +0200

    lesson end

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            // Solution
-            description: 'A warm fuzzy pair of socks.' 
-            // Solution
+            description: 'A pair of socks',
         }
     }
 })
