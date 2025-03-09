3b8d49110ae6ca3d61016922e48def4c558c52ab
Juho Lehto <284658+lehtoj@users.noreply.github.com>
2025-03-09 11:07:08 +0200

    end

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
+            inventory: 8,
+            onSale: true,
         }
     }
 })
